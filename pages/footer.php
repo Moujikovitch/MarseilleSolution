46dcd753c9086155d26291be0ba9cff47b1c0cd6
moujikovitch <sbst.legall@gmail.com>
2015-07-07 17:55:08 +0200

    premier upload

---
+++ pages/footer.php
@@ -0,0 +1,8 @@
+	<div id="footer">
+		<div class="cont">
+			<a href="https://www.facebook.com/marseille.solutions?fref=ts" target="_blank">
+				<div id="fblogo">
+				</div>
+			</a>
+		</div>
+	</div>
---
+++ pages/quisommes.php
@@ -0,0 +1,46 @@
+	<div id="milieu">
+		<div id="slideshow">
+    		<div id="sContent">
+    			<div id="imgs0" class="imgslider">
+                    <div id="txts0" class="txts">Texte 1</div>
+    			</div>
+    			<div id="imgs1" class="imgslider">
+                    <div id="txts0" class="txts">Texte 2</div>
+    			</div>
+    			<div id="imgs2" class="imgslider">
+                    <div id="txts0" class="txts">Texte 3</div>
+    			</div>
+    		</div>
+		</div>
+        <div id="contbtnslider">
+            <div id="btns0" class="btnslider">
+            </div>
+            <div id="btns1" class="btnslider">
+            </div>
+            <div id="btns2" class="btnslider">
+            </div>
+        </div>
+	</div>
+    <script>
+        var etape = 0
+        function slide() {
+            etape++;
+            if (etape==3){
+                etape=0;
+            };
+            var leftv = etape*-120;
+            $("#sContent").animate({
+            left:leftv.toString()+"vh",
+            }, 2000,"swing");
+        };
+        var next = setInterval(function(){slide();},5000);
+        $(".btnslider").click(function(){
+            clearInterval(next);
+           etape = parseInt($(this.id).selector.substring(4,5));
+           var leftv = etape*-120;
+           $("#sContent").animate({
+            left:leftv.toString()+"vh",
+            }, 200,"swing");
+           next = setInterval(function(){slide();},5000);
+        })
+    </script>
